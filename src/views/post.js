796bdf182c49fa859c3a8669a5118edaa370fb6f
Jojo Chen <jojochen@berkeley.edu>
2022-11-05 16:33:38 -0700

    Post page UI

--- src/views/post.js
+++ src/views/post.js
@@ -1,147 +1,73 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Text, Flex, Box, Button, Badge} from '@chakra-ui/react'
+import { Text, Flex, Box, Button, Badge, Input, FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText, Textarea, Container, ChakraProvider, extendTheme} from '@chakra-ui/react'
 import {
     LivepeerConfig,
     createReactClient,
-    studioProvider,
+    studioProvider, 
   } from '@livepeer/react';
   import * as React from 'react';
   import { useDropzone } from 'react-dropzone';
   import {useState, useCallback, useMemo, useRef} from 'react';
   import { Player, useAsset, useCreateAsset, useAssetMetrics } from '@livepeer/react';
 
-function Post() {
+  export default function Post() {
+    const colors = {
 
+  brand: {
+    black: '#151514',
+    lightyellow: '#FEFDF9',
+    white: '#FFFFFF',
+    green: '#E6FC9C',
+    purple:'8E84EF',
 
-  const livepeerClient = createReactClient({
-    provider: studioProvider({
-      apiKey: process.env.NEXT_PUBLIC_STUDIO_API_KEY,
-    }),
-  });
-    const [video, setVideo] = useState();
-    const {
-      mutate: createAsset,
-      data: createdAsset,
-      status: createStatus,
-      error: createError,
-      uploadProgress,
-    } = useCreateAsset();
+  },
+}
+const styles = {
+  global: (props) => ({
+    body:{
+      // bg: props.colorMode === "dark" ? "brand.black" : "brand.lightyellow",
+      bg: "brand.lightyellow",
+      color: "brand.black",
+    },
+  })
+}
+const theme = extendTheme({ colors, styles})
 
-    const { data: asset, status: assetStatus } = useAsset({
-        assetId: createdAsset?.id,
-        refetchInterval: (asset) =>
-          asset?.status?.phase !== 'ready' ? 5000 : false,
-      });
-    const { data: metrics } = useAssetMetrics({
-        assetId: createdAsset?.id,
-        refetchInterval: 30000,
-      });
- 
-    const onDrop = useCallback(async (acceptedFiles) => {
-        if (acceptedFiles && acceptedFiles.length > 0 && acceptedFiles?.[0]) {
-          setVideo(acceptedFiles[0]);
-        }
-      }, []);
+    let [value, setValue] = React.useState('')
+    const handlePostCreation = (e) => {
+      e.preventDefault();
+      const description = e.target.description.value;
+      // console.log(description);
+    };
+    let handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setValue(inputValue)
+  }
 
-      const { getRootProps, getInputProps } = useDropzone({
-        accept: {
-          'video/*': ['*.mp4'],
-        },
-        maxFiles: 1,
-        onDrop,
-      });    
-
-
-      const progressFormatted = useMemo(
-        () =>
-          uploadProgress
-            ? `Uploading: ${Math.round(uploadProgress * 100)}%`
-            : asset?.status?.progress
-            ? `Processing: ${Math.round(asset?.status?.progress * 100)}%`
-            : null,
-        [uploadProgress, asset?.status?.progress],
-      );
-
-      const inputRef = useRef(null);
-
-      const handleClick = () => {
-        // 👇️ open file input box on click of other element
-        inputRef.current.click();
-      };
-    
-      const handleFileChange = event => {
-        const fileObj = event.target.files && event.target.files[0];
-        if (!fileObj) {
-          return;
-        }
-    
-        console.log('fileObj is', fileObj);
-    
-        // 👇️ reset file input
-        event.target.value = null;
-    
-        // 👇️ is now empty
-        console.log(event.target.files);
-    
-        // 👇️ can still access file object here
-        console.log(fileObj);
-        console.log(fileObj.name);
-      };
-
-    return (
-        /*
-        <>
-        <Flex>
-            <Text fontSize='50px'>
-                Post
-            </Text>
-            <ConnectButton chainStatus={'none'}/>
-        </Flex>
-         
-         <Box {...getRootProps()}>
-           <Box as="input" {...getInputProps()} />
-           <Box as="p">
-             <Text>Drag and drop or browse files</Text>
-           </Box>
-         </Box>
-    
-         {createError?.message && <Text>{createError.message}</Text>}
-    
-         {video ? (
-           <Badge>{video.name}</Badge>
-         ) : (
-           <Text>Select a video file to upload.</Text>
-         )}
-         {progressFormatted && <Text>{progressFormatted}</Text>}
-    
-         <Button
-
-           onClick={() => {
-
-             if (video) {
-               createAsset({ name: video.name, file: video });
-             }
-           }}
-           //disabled={!video || createStatus === 'loading'}
-         >
-           Upload
-         </Button>
-       </>
-       */
-
-
-    <div>
-      <input
-        style={{display: 'none'}}
-        ref={inputRef}
-        type="file"
-        onChange={handleFileChange}
-      />
-
-      <button onClick={handleClick}>Open file upload box</button>
-    </div>
-  );
-
-    }
-
-export default Post;
+    return(
+      <ChakraProvider theme={theme}>
+      <>
+      <Box width='100%' pl='15%'>
+        <Flex direction="column">
+        <Text fontSize='3xl' as='b' pb='2rem'>Create Post</Text>
+        <FormControl>
+          <FormLabel fontSize='2xl' as='b'>Upload Media</FormLabel>
+          <Text fontSize="md" color='black'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
+      </FormControl>
+      <Button mb='3rem' mt='1rem' bg='brand.white' color='brand.black' border='1px' borderColor='black' variant='outline' width='200px' sx={{borderRadius:"50px"}} _hover={{ borderColor: 'purple.500', color: 'purple.500'}} >Upload Video</Button>
+      <FormControl>
+        <FormLabel fontSize='2xl' as='b'>Description</FormLabel>
+        <Text fontSize="md" pb='1rem'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
+        <Text fontSize='md'>{value}</Text>
+        <Textarea placeholder='Input a description of your video...' size="md" w="60%" bg='white' border='1px' borderColor='black' color='black'/>
+      </FormControl>
+      <Button mt='2rem' bg='brand.black' color='brand.white' width='200px' _hover={{ bg: 'purple.500' }}sx={{borderRadius:"50px"}}>Post</Button>
+      </Flex>
+      </Box>
+      </>
+      </ChakraProvider>
+    );
+  }
