f70eaac8a169a49fac6521d8e1e6d22557de08de
Jojo Chen <jojochen@berkeley.edu>
2022-11-05 18:56:17 -0700

    idk what this is

--- src/views/post.js
+++ src/views/post.js
@@ -50,21 +50,21 @@ const theme = extendTheme({ colors, styles})
     return(
       <ChakraProvider theme={theme}>
       <>
-      <Box width='100%' pl='15%'>
-        <Flex direction="column">
-        <Text fontSize='3xl' as='b' pb='2rem'>Create Post</Text>
+      <Box width='100%' pl='15%' zIndex="2">
+        <Flex direction="column" zIndex="2">
+        <Text fontSize='3xl' as='b' pb='2rem' zIndex="2">Create Post</Text>
         <FormControl>
-          <FormLabel fontSize='2xl' as='b'>Upload Media</FormLabel>
-          <Text fontSize="md" color='black'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
+          <FormLabel fontSize='2xl' as='b' zIndex="2">Upload Media</FormLabel>
+          <Text fontSize="md" color='black' zIndex="2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
       </FormControl>
-      <Button mb='3rem' mt='1rem' bg='brand.white' color='brand.black' border='1px' borderColor='black' variant='outline' width='200px' sx={{borderRadius:"50px"}} _hover={{ borderColor: 'purple.500', color: 'purple.500'}} >Upload Video</Button>
+      <Button zIndex="2" mb='3rem' mt='1rem' bg='brand.white' color='brand.black' border='1px' borderColor='black' variant='outline' width='200px' sx={{borderRadius:"50px"}} _hover={{ borderColor: 'purple.500', color: 'purple.500'}} >Upload Video</Button>
       <FormControl>
-        <FormLabel fontSize='2xl' as='b'>Description</FormLabel>
-        <Text fontSize="md" pb='1rem'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
-        <Text fontSize='md'>{value}</Text>
-        <Textarea placeholder='Input a description of your video...' size="md" w="60%" bg='white' border='1px' borderColor='black' color='black'/>
+        <FormLabel zIndex="2" fontSize='2xl' as='b'>Description</FormLabel>
+        <Text zIndex="2" fontSize="md" pb='1rem'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</Text>
+        <Text zIndex="2" fontSize='md'>{value}</Text>
+        <Textarea zIndex="2" _placeholder='Input a description of your video...' size="md" w="60%" bg='white' border='1px' borderColor='black' color='black'/>
       </FormControl>
-      <Button mt='2rem' bg='brand.black' color='brand.white' width='200px' _hover={{ bg: 'purple.500' }}sx={{borderRadius:"50px"}}>Post</Button>
+      <Button zIndex="2" mt='2rem' bg='brand.black' color='brand.white' width='200px' _hover={{ bg: 'purple.500' }}sx={{borderRadius:"50px"}}>Post</Button>
       </Flex>
       </Box>
       </>
